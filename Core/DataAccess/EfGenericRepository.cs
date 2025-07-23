cec30d2dec15c35b4b4a199ed290bb7ba476d5a9
Meryem Aydın <aydnn.meryem@gmail.com>
2025-07-23 18:08:38 +0300

    Core Layer GenericRepositories was created

---
+++ Core/DataAccess/EfGenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.DataAccess
+{
+    public class EfGenericRepository<TEntity, TContext> : IGenericRepository<TEntity> where TEntity : class, IEntity,new()
+      where TContext : DbContext
+    {
+        protected readonly TContext Context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public EfGenericRepository(TContext context)
+        {
+            Context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task<bool> AnyAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _dbSet.AnyAsync(filter);
+        }
+
+        public IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            return filter == null ? _dbSet : _dbSet.Where(filter);
+        }
+
+        public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            return await _dbSet.FirstOrDefaultAsync(filter);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
---
+++ ResumeProject.DataAccess/Contexts/ResumeProjectDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ResumeProject.Entity.Concrete;
+
+namespace ResumeProject.DataAccess.Contexts
+{
+    public sealed class ResumeProjectDbContext : DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Certificate> Certificates { get; set; }
+        public DbSet<Contact> Contacts {  get; set; }
+        public DbSet<Education> Educations { get; set; }    
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Interest> Interests { get; set; }
+        public DbSet<Language> languages { get; set; }
+        public DbSet<PersonalInfo> PersonalInfos { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialAccount> SocialAccounts { get; set; }
+
+    }
+}
