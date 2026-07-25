e8e6617e97d4c8feff44bfa3ecb445a417af70c9
Meryem Aydın <aydnn.meryem@gmail.com>
2026-07-25 08:28:48 +0300

    Update Program.cs

--- ResumeProject.WebApi/Program.cs
+++ ResumeProject.WebApi/Program.cs
@@ -56,5 +56,9 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
 app.Run();
